37111e65b4b69e929c27bac3c5f984149a7cd7a3
Erfan <erfansedighi84@gmail.com>
2025-05-05 07:14:39 +0330

    update footer

--- src/components/ui/MainFooter.tsx
+++ src/components/ui/MainFooter.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const MainFooter = () => {
   return (
     <footer className="bg-white absolute bottom-0 w-full">
-      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-center space-x-6">
+      <div className="max-w-7xl mx-auto py-6 px-4 min-h-24 sm:px-6 lg:px-8">
+        {/* <div className="flex justify-center space-x-6">
           <a href="#" className="text-gray-400 hover:text-gray-500">
             <span className="sr-only">فیسبوک</span>
             <svg
@@ -46,10 +46,10 @@ const MainFooter = () => {
               <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
             </svg>
           </a>
-        </div>
-        <p className="mt-4 text-center text-sm text-gray-500">
+        </div> */}
+        {/* <p className="mt-4 text-center text-sm text-gray-500">
           © ۲۰۲۴ مدرسه شبانه. کلیه حقوق محفوظ است.
-        </p>
+        </p> */}
       </div>
     </footer>
   );
